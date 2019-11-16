6fb694d823d0bdd471d3edbeee4b6c24fd70e673
rohitganguly <rohitganguly88@gmail.com>
2019-11-16 17:27:13 -0500

    working responses

--- public/electron.js
+++ public/electron.js
@@ -8,9 +8,13 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+// must set nodeIntegration to true because electron must allow node code to run
+  mainWindow = new BrowserWindow({width: 400, height: 712, webPreferences: {
+  nodeIntegration: true
+  }} );
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
+    // detects if this is running at localhost3000, in which case we are developing and we can do things like open webtools.
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
     mainWindow.webContents.openDevTools();
--- src/App.js
+++ src/App.js
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import './static/css/chat_interface.css';
+//import './static/css/temporary.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js bongo</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SendButton extends Component{
+    render(){
+      return (<div className="send_message" onClick={this.props.handleClick}>
+                <div className="text">send</div>
+              </div>);
+    }
 }
 
-export default App;
+class MessageTextBoxContainer extends Component{
+  render(){
+    return(
+      <div className="message_input_wrapper">
+        <input id="msg_input" className="message_input" placeholder="Type your messages here..." value={this.props.message} onChange={this.props.onChange} onKeyPress={this.props._handleKeyPress}/>
+      </div>
+    );
+  }
+}
+
+class Avatar extends Component {
+  render(){
+    return(
+      <div className="avatar"/>
+    );
+  }
+}
+
+class BotMessageBox extends Component{
+
+  //constructor(props) {
+    //super(props);
+  //}
+  render(){
+    return(
+      <li className="message left appeared">
+        <Avatar></Avatar>
+        <div className="text_wrapper">
+            <div className="text">{this.props.message}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class UserMessageBox extends Component{
+  //constructor(props) {
+    //super(props);
+  //}
+  render(){
+    return(
+      <li className={`message ${this.props.appearance} appeared`}>
+        <Avatar></Avatar>
+        <div className="text_wrapper">
+            <div className="text">{this.props.message}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class MessagesContainer extends Component{
+  constructor(props) {
+    super(props);
+    this.createBotMessages = this.createBotMessages.bind(this);
+  }
+
+  scrollToBottom = () => {
+    var el = this.refs.scroll;
+    el.scrollTop = el.scrollHeight;
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  createBotMessages(){
+    console.log(this.props.messages);
+    return this.props.messages.map((message, index) =>
+       <UserMessageBox key={index} message={message["message"]} appearance={message["isbotmessage"] ? "left": "right"}/>
+    );
+  }
+
+  render(){
+
+    return(
+      <ul className="messages" ref="scroll">
+        {this.createBotMessages()}
+      </ul>
+    );
+  }
+}
+
+
+class ChatApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {"messages": [], "current_message":""}
+    this.handleClick = this.handleClick.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.addMessageBox = this.addMessageBox.bind(this);
+  }
+
+
+  addMessageBox(enter=true){
+    let messages = this.state.messages;
+    let current_message = this.state.current_message;
+    console.log(this.state);
+    if(current_message && enter){
+      messages = [...messages, {"message":current_message}];
+      fetch("https://random-word-api.herokuapp.com/word?key=T0OZ8JHD&number=10")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          //alert(result);
+          //console.log(result);
+          // for values in results: set state to results[i] or whatevah or concat messages
+          let appended_message = "";
+          Object.keys(result).forEach(function(key) {
+              appended_message = appended_message + " " + result[key]
+          });
+          this.setState({
+            messages: [...messages, {"message":appended_message + " ", "isbotmessage":true}]
+        });
+        },
+        (error) => {
+          //do nothing for now
+          this.setState({
+            messages: [...messages, {"message":"I couldn't access the API, sorry :(", "isbotmessage":true}]
+          });
+        }
+      );
+      current_message = ""
+    }
+    this.setState({
+      current_message: current_message,
+      messages
+    });
+
+  }
+
+  handleClick(){
+    this.addMessageBox();
+  }
+
+  onChange(e) {
+    this.setState({
+      current_message: e.target.value
+    });
+  }
+
+    _handleKeyPress(e) {
+    let enter_pressed = false;
+    if(e.key === "Enter"){
+      enter_pressed = true;
+    }
+    this.addMessageBox(enter_pressed)
+  }
+
+  render() {
+    return (
+      <div className="chat_window">
+        <div className = "message_divider">
+            <MessagesContainer messages={this.state.messages}></MessagesContainer>
+        </div>
+        <div className="bottom_wrapper clearfix">
+          <MessageTextBoxContainer
+            _handleKeyPress={this._handleKeyPress}
+            onChange={this.onChange}
+            message={this.state.current_message}></MessageTextBoxContainer>
+          <SendButton handleClick={this.handleClick}></SendButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ChatApp;
